45f8b7a8dc70661c95598b5e8ecb1aee8ff6c1d8
TedwardJ <edt77@ukr.net>
2020-12-07 22:47:01 +0200

    DynamicClassLoader changes: - order in finding classes: if class is not loaded yet, the loader firstly tries to load it by itself, and, then, if not found, by means of parend ClassLoader

--- src/main/java/edu/ted/servlethandler/entity/DynamicClassLoader.java
+++ src/main/java/edu/ted/servlethandler/entity/DynamicClassLoader.java
@@ -1,18 +1,49 @@
 package edu.ted.servlethandler.entity;
 
-import lombok.Getter;
-
 import java.net.URL;
 import java.net.URLClassLoader;
 
 
 public class DynamicClassLoader extends URLClassLoader {
 
-    @Getter
     private final URL[] classPath;
 
     public DynamicClassLoader(URL[] classPath, ClassLoader parent) {
         super(classPath, parent);
         this.classPath = classPath;
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve)
+            throws ClassNotFoundException {
+        synchronized (getClassLoadingLock(name)) {
+            // First, check if the class has already been loaded
+            Class<?> c = findLoadedClass(name);
+            if (c == null) {
+                try {
+                    c = super.findClass(name);
+                } catch (ClassNotFoundException e) {
+                    // ClassNotFoundException thrown if class not found
+                    // from the non-null parent class loader
+                }
+            }
+            if (c == null) {
+                // If still not found, then invoke parent findClass in order
+                // to find the class.
+                try {
+                    if (getParent() != null) {
+                        c = getParent().loadClass(name);
+                    }
+                } catch (ClassNotFoundException e) {
+                    // ClassNotFoundException thrown if class not found
+                    // from the non-null parent class loader
+                }
+
+            }
+            if (resolve) {
+                resolveClass(c);
+            }
+            return c;
+        }
     }
 }
